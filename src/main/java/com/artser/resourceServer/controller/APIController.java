756ed14c0b7526794cf7e80e02192a64d0b08798
artser97 <artser97@yandex.ru>
2022-12-14 14:53:00 +0200

    add roles

--- src/main/java/com/artser/resourceServer/controller/APIController.java
+++ src/main/java/com/artser/resourceServer/controller/APIController.java
@@ -2,11 +2,14 @@ package com.artser.resourceServer.controller;
 
 import com.artser.resourceServer.data.Message;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4040")
 @RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class APIController {
 
@@ -14,5 +17,18 @@ public class APIController {
   @GetMapping(value = "/private")
   public Message privateEndpoint() {
     return new Message("All good. You can see this because you are Authenticated.");
+  }
+
+  @GetMapping(value ="/admin")
+  @PreAuthorize("hasAuthority('read:all')")
+  public Message getAdmin() {
+    return new Message("All good. You can see this because you are Admin.");
+  }
+
+
+  @GetMapping(value ="/read")
+  @PreAuthorize("hasAuthority('read:messages')")
+  public Message getRead() {
+    return new Message("All good. You can see this because you have Read permissions.");
   }
 }
--- src/main/java/com/artser/resourceServer/security/SecurityConfig.java
+++ src/main/java/com/artser/resourceServer/security/SecurityConfig.java
@@ -10,11 +10,6 @@ import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 /**
  * Configures our application with Spring Security to restrict access to our API endpoints.
@@ -25,20 +20,18 @@ public class SecurityConfig {
 
   @Value("${auth0.audience}")
   private String audience;
-
   @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
   private String issuer;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .csrf().disable()
         .authorizeHttpRequests()
         .requestMatchers(HttpMethod.OPTIONS, "/**")
         .permitAll()
-        .requestMatchers("/api/private").authenticated()
-        .and().oauth2ResourceServer().jwt();
+        .requestMatchers("/api/private", "/api/admin").authenticated()
+        .and().oauth2ResourceServer().jwt().decoder(jwtDecoder());
     return http.build();
   }
 
@@ -47,26 +40,10 @@ public class SecurityConfig {
 
     NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
         JwtDecoders.fromOidcIssuerLocation(issuer);
-
     OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
     OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
     OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
     jwtDecoder.setJwtValidator(withAudience);
-
     return jwtDecoder;
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedHeaders(List.of("*"));
-    configuration.setAllowedOrigins(List.of("http://localhost:4040", "http://127.0.0.1:4040"));
-    configuration.setAllowedMethods(List.of("*"));
-    configuration.setAllowCredentials(false);
-    configuration.setMaxAge(-1L);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
   }
 }
